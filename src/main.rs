fbe8ab9aff5394950271b81346188758bb0dc43b
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-01-30 19:54:02 -0600

    pipeling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{Write, BufWriter, Result};
 use indoc::indoc;
 
@@ -8,40 +8,53 @@ struct VhdlWriter<'a> {
     n: usize,
     data_width: usize,
     wires: Vec<String>,
+    intermediate_regs: Vec<String>,
     inputs: Vec<String>,
-    writer: BufWriter<&'a File>, 
+    writer: BufWriter<&'a File>,
 }
 
 impl<'a> VhdlWriter<'a> {
     fn new(n: usize, data_width: usize, file: &'a File) -> Self {
-        let total_comparators = ((n as f32).log2() * ((n as f32).log2() + 1.0) * (n as f32) / 4.0) as i32;
-        
+        let log_n = (n as u32).ilog2();
+        let num_stages = (log_n * (log_n + 1)) / 2;
+        let total_comparators = (n * num_stages as usize) / 2;
+
         let mut wires: Vec<String> = vec![];
+        let mut intermediate_regs: Vec<String> = vec![];
+
         for i in 0..total_comparators {
             wires.push(format!("w_l_{i}"));
             wires.push(format!("w_h_{i}"));
+            intermediate_regs.push(format!("ir_l_{i}"));
+            intermediate_regs.push(format!("ir_h_{i}"));
         }
-
         let mut inputs: Vec<String> = vec![];
         for i in 0..n {
             inputs.push(format!("inputs({})", i));
         }
-
         VhdlWriter {
+            id: 0,
             n,
             data_width,
-            id: 0,
             wires,
+            intermediate_regs,
             inputs,
             writer: BufWriter::new(file),
         }
     }
-
-    fn add_comparator(&mut self, dir: u32, in_a: &str, in_b: &str, out_l: &str, out_h: &str) -> Result<()> {
+    fn write_comparator(
+        writer: &mut BufWriter<&'a File>,
+        id: usize,
+        dir: u32,
+        in_a: &str,
+        in_b: &str,
+        out_l: &str,
+        out_h: &str
+    ) -> Result<()> {
         writeln!(
-            self.writer,
+            writer,
             "    comp_{id}: entity work.comparator port map(in_A => {in_a}, in_B => {in_b}, dir => '{dir}', out_L => {out_l}, out_H => {out_h});",
-            id = self.id, in_a = in_a, in_b = in_b, dir = dir, out_l = out_l, out_h = out_h
+            id = id, in_a = in_a, in_b = in_b, dir = dir, out_l = out_l, out_h = out_h
         )?;
         Ok(())
     }
@@ -49,6 +62,9 @@ impl<'a> VhdlWriter<'a> {
     fn write_wires(&mut self) -> Result<()> {
         for w in &self.wires {
             writeln!(self.writer, "    signal {} : std_logic_vector(width - 1 downto 0);", w)?;
+        }
+        for r in &self.intermediate_regs {
+            writeln!(self.writer, "    signal {} : std_logic_vector(width - 1 downto 0);", r)?;
         }
         writeln!(self.writer, "begin")?;
         Ok(())
@@ -68,8 +84,9 @@ impl<'a> VhdlWriter<'a> {
                         width : integer := {w}
                     );
                     Port (
-                        inputs  : in mem(0 to n - 1)(width - 1 downto 0); 
-                        outputs : out  mem(0 to n - 1)(width - 1 downto 0)       
+                        clk: in std_logic;
+                        inputs  : in mem(0 to n - 1)(width - 1 downto 0);
+                        outputs : out  mem(0 to n - 1)(width - 1 downto 0)
                     );
                 end bitonic_network;
 
@@ -85,9 +102,24 @@ impl<'a> VhdlWriter<'a> {
     }
 
     fn write_end(&mut self) -> Result<()> {
+        let mut assign_statements: Vec<String> = vec![];
+        for (i, wire) in self.wires.iter().enumerate() {
+            assign_statements.push(format!("{} <= {};", self.intermediate_regs[i], wire ));
+        }
+        let as_statements_init_string = assign_statements.join("\n        ");
+
+        let process = format!( indoc!{r#"
+                process(clk)
+                begin
+                    if rising_edge(clk) then
+                        {statements}
+                    end if;
+                end process;
+        "#},statements = as_statements_init_string);
         for (i, output) in self.inputs.iter().enumerate() {
             writeln!(self.writer,"    outputs_array({}) <= {};", i, output)?;
         }
+        self.writer.write_all(process.as_bytes())?;
         let end = indoc! {b"
                 outputs <= outputs_array;
             end Behavioral;
@@ -101,16 +133,22 @@ fn bitonic_merge(vhdl_writer: &mut VhdlWriter, count: usize, low: usize, dir: u3
     if count > 1 {
         let k = count / 2;
         for i in low..low + k {
-            let out_l = vhdl_writer.wires[vhdl_writer.id * 2].clone();
-            let out_h = vhdl_writer.wires[vhdl_writer.id * 2 + 1].clone();
-            let in_a = vhdl_writer.inputs[i].clone();
-            let in_b = vhdl_writer.inputs[i + k].clone();
-
-            vhdl_writer.add_comparator(dir, &in_a, &in_b, &out_l, &out_h)?;
-
-            vhdl_writer.inputs[i] = vhdl_writer.wires[vhdl_writer.id * 2].clone();
-            vhdl_writer.inputs[i + k] = vhdl_writer.wires[vhdl_writer.id * 2 + 1].clone();
-            
+            let id = vhdl_writer.id;
+            let out_l = format!("w_l_{}", id);
+            let out_h = format!("w_h_{}", id);
+            let in_a = &vhdl_writer.inputs[i];
+            let in_b = &vhdl_writer.inputs[i + k];
+            VhdlWriter::write_comparator(
+                &mut vhdl_writer.writer,
+                id,
+                dir,
+                in_a,
+                in_b,
+                &out_l,
+                &out_h
+            )?;
+            vhdl_writer.inputs[i] = format!("ir_l_{}", id);
+            vhdl_writer.inputs[i + k] = format!("ir_h_{}", id);
             vhdl_writer.id += 1;
         }
         bitonic_merge(vhdl_writer, k, low, dir)?;
@@ -132,14 +170,13 @@ fn bitonic_sort_helper(vhdl_writer: &mut VhdlWriter, count: usize, low: usize, d
 fn generate_sorter_file(n: usize, width: usize) -> Result<()> {
     let mut file = File::create("vhdl/sorter.vhd")?;
     let mut rng = rand::rng();
-    
     let mut random_numbers_vhdl = Vec::new();
     let max_val = (1u32 << width) - 1;
 
     println!("--- Generated numbers ---");
     for _ in 0..n {
         let num = rng.random_range(0..=max_val);
-        print!("{} ", num); 
+        print!("{} ", num);
         random_numbers_vhdl.push(format!("std_logic_vector(to_unsigned({}, {}))", num, width));
     }
     println!("\n-----------------------------------");
@@ -162,27 +199,27 @@ fn generate_sorter_file(n: usize, width: usize) -> Result<()> {
             end sorter;
 
             architecture Behavioral of sorter is
-                
+
                 constant N_SYSTEM : integer := {n};
                 constant W_SYSTEM : integer := {w};
 
                 signal inputs : mem(0 to N_SYSTEM - 1)(W_SYSTEM - 1 downto 0) := (
                     {inputs_str}
                 );
-                
+
                 signal outputs : mem(0 to N_SYSTEM - 1)(W_SYSTEM - 1 downto 0) := (others => (others => '0'));
                 signal enabled: std_logic := '0';
                 signal idx: integer range 0 to N_SYSTEM - 1 := 0;
                 signal led_reg: std_logic_vector(15 downto 0) := (others => '0');
 
             begin
-                network: entity work.bitonic_network 
+                network: entity work.bitonic_network
                 generic map (
                     n => N_SYSTEM,
                     width => W_SYSTEM
                 )
                 port map(
-                    inputs => inputs, 
+                    inputs => inputs,
                     outputs => outputs
                 );
 
@@ -192,18 +229,18 @@ fn generate_sorter_file(n: usize, width: usize) -> Result<()> {
                 begin
                     if rising_edge(CLK100MHZ) then
                         if enabled = '1' then
-                          
+
                             led_reg <= std_logic_vector(resize(unsigned(outputs(idx)), 16));
-                            
+
                             if idx = N_SYSTEM - 1 then
-                                idx <= 0;    
+                                idx <= 0;
                             else
                                 idx <= idx + 1;
                             end if;
                         end if;
                     end if;
                 end process;
-                
+
                 LED <= led_reg;
             end Behavioral;
         "#},
@@ -215,6 +252,7 @@ fn generate_sorter_file(n: usize, width: usize) -> Result<()> {
     file.write_all(content.as_bytes())?;
     Ok(())
 }
+
 fn generate_comparator_file(width: usize) -> Result<()> {
     let mut file = File::create("vhdl/comparator.vhd")?;
     let content = format!(
@@ -240,19 +278,19 @@ fn generate_comparator_file(width: usize) -> Result<()> {
             begin
                 process(in_A, in_B, dir)
                 begin
-                
+
                     out_L <= in_A;
                     out_H <= in_B;
 
-                    if (dir = '1' and unsigned(in_A) > unsigned(in_B)) then 
+                    if (dir = '1' and unsigned(in_A) > unsigned(in_B)) then
                         out_L <= in_B;
                         out_H <= in_A;
-                        
-                    elsif (dir = '0' and unsigned(in_A) < unsigned(in_B)) then 
+
+                    elsif (dir = '0' and unsigned(in_A) < unsigned(in_B)) then
                         out_L <= in_B;
                         out_H <= in_A;
                     end if;
-                    
+
                 end process;
             end Behavioral;
         "#},
@@ -262,13 +300,16 @@ fn generate_comparator_file(width: usize) -> Result<()> {
     file.write_all(content.as_bytes())?;
     Ok(())
 }
+
 fn main() -> Result<()> {
     let n = 8;
     let data_width = 16;
 
+    fs::create_dir_all("vhdl")?;
+
     let file_network = File::create("vhdl/bitonic_network.vhd")?;
     let mut vhdl_writer = VhdlWriter::new(n, data_width, &file_network);
-    
+
     vhdl_writer.write_start()?;
     vhdl_writer.write_wires()?;
     bitonic_sort_helper(&mut vhdl_writer, n as usize, 0, 1)?;
