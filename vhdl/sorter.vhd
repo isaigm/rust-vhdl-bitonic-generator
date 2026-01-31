6f99568255d5da453fe5c4a766410e2b48885408
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-01-30 19:59:49 -0600

    added clk

--- vhdl/sorter.vhd
+++ vhdl/sorter.vhd
@@ -12,34 +12,35 @@ entity sorter is
 end sorter;
 
 architecture Behavioral of sorter is
-    
+
     constant N_SYSTEM : integer := 8;
     constant W_SYSTEM : integer := 16;
 
     signal inputs : mem(0 to N_SYSTEM - 1)(W_SYSTEM - 1 downto 0) := (
-        std_logic_vector(to_unsigned(24470, 16)),
-        std_logic_vector(to_unsigned(32931, 16)),
-        std_logic_vector(to_unsigned(29393, 16)),
-        std_logic_vector(to_unsigned(3328, 16)),
-        std_logic_vector(to_unsigned(7941, 16)),
-        std_logic_vector(to_unsigned(64973, 16)),
-        std_logic_vector(to_unsigned(47855, 16)),
-        std_logic_vector(to_unsigned(41532, 16))
+        std_logic_vector(to_unsigned(34613, 16)),
+        std_logic_vector(to_unsigned(24644, 16)),
+        std_logic_vector(to_unsigned(47904, 16)),
+        std_logic_vector(to_unsigned(56099, 16)),
+        std_logic_vector(to_unsigned(17938, 16)),
+        std_logic_vector(to_unsigned(64379, 16)),
+        std_logic_vector(to_unsigned(30941, 16)),
+        std_logic_vector(to_unsigned(31002, 16))
     );
-    
+
     signal outputs : mem(0 to N_SYSTEM - 1)(W_SYSTEM - 1 downto 0) := (others => (others => '0'));
     signal enabled: std_logic := '0';
     signal idx: integer range 0 to N_SYSTEM - 1 := 0;
     signal led_reg: std_logic_vector(15 downto 0) := (others => '0');
 
 begin
-    network: entity work.bitonic_network 
+    network: entity work.bitonic_network
     generic map (
         n => N_SYSTEM,
         width => W_SYSTEM
     )
     port map(
-        inputs => inputs, 
+        clk => CLK100MHZ,
+        inputs => inputs,
         outputs => outputs
     );
 
@@ -49,17 +50,17 @@ begin
     begin
         if rising_edge(CLK100MHZ) then
             if enabled = '1' then
-              
+
                 led_reg <= std_logic_vector(resize(unsigned(outputs(idx)), 16));
-                
+
                 if idx = N_SYSTEM - 1 then
-                    idx <= 0;    
+                    idx <= 0;
                 else
                     idx <= idx + 1;
                 end if;
             end if;
         end if;
     end process;
-    
+
     LED <= led_reg;
 end Behavioral;
