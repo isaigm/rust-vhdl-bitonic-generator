d3e9801c35c348889fb632c43b6bb9704987257f
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-02-18 23:16:43 -0600

    Refactor push_btn entity to use clk and debounce button

--- vhdl/push_btn.vhd
+++ vhdl/push_btn.vhd
@@ -4,33 +4,42 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity push_btn is
     Port ( 
-        CLK100MHZ : in  std_logic; 
-        btn      : in  std_logic; 
-        enabled   : out std_logic
+        clk     : in  std_logic; 
+        btn     : in  std_logic; 
+        enabled : out std_logic
     );
 end push_btn;
 
 architecture Behavioral of push_btn is
-    signal s_enabled : std_logic := '0';
-    signal btn_sync_0 : std_logic := '0'; 
-    signal btn_sync_1 : std_logic := '0'; 
-    signal btn_last   : std_logic := '0'; 
+    signal btn_sync_0, btn_sync_1 : std_logic := '0';
+    signal count : unsigned(19 downto 0) := (others => '0');
+    signal btn_stable : std_logic := '0';
+    signal btn_last   : std_logic := '0';
 begin
 
-    process (CLK100MHZ)
+    process (clk)
     begin
-        if rising_edge(CLK100MHZ) then
-            btn_sync_0 <= btn;     
-            btn_sync_1 <= btn_sync_0; 
-            btn_last   <= btn_sync_1;
-            s_enabled <= '0'; 
-            if btn_sync_1 = '1' and btn_last = '0' then
-                s_enabled <= '1';
+        if rising_edge(clk) then
+
+            btn_sync_0 <= btn;
+            btn_sync_1 <= btn_sync_0;
+
+            if btn_sync_1 /= btn_stable then
+                count <= count + 1;
+                if count = 1000000 then 
+                    btn_stable <= btn_sync_1;
+                    count <= (others => '0');
+                end if;
+            else
+                count <= (others => '0');
             end if;
-            
+
+            btn_last <= btn_stable;
+            enabled <= '0';
+            if btn_stable = '1' and btn_last = '0' then
+                enabled <= '1';
+            end if;
         end if;
     end process;
-
-    enabled <= s_enabled;
 
 end Behavioral;
