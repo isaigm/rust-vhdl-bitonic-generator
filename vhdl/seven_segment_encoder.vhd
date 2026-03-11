966e9f295dfaedf169aab815bf145e5f9d38f014
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-03-11 17:13:17 -0600

    Refactor seven_segment_encoder to remove enable signal
    Removed enable signal and updated segment encoding to handle hexadecimal digits.

--- vhdl/seven_segment_encoder.vhd
+++ vhdl/seven_segment_encoder.vhd
@@ -1,35 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 entity seven_segment_encoder is
     Port (
-        enable: in std_logic := '0';
-        digit: in std_logic_vector(3 downto 0) := "0000";
+        digit    : in std_logic_vector(3 downto 0);
         segments : out std_logic_vector(6 downto 0)
      );
 end seven_segment_encoder;
 
 architecture Behavioral of seven_segment_encoder is
 begin
-    process(digit, enable)
+    process(digit)
     begin
-        if enable = '1' then
-            case digit is
-                when "0000" => segments <= "1000000"; -- 0
-                when "0001" => segments <= "1111001"; -- 1
-                when "0010" => segments <= "0100100"; -- 2
-                when "0011" => segments <= "0110000"; -- 3
-                when "0100" => segments <= "0011001"; -- 4
-                when "0101" => segments <= "0010010"; -- 5
-                when "0110" => segments <= "0000010"; -- 6
-                when "0111" => segments <= "1111000"; -- 7
-                when "1000" => segments <= "0000000"; -- 8
-                when "1001" => segments <= "0010000"; -- 9
-                when others => segments <= "1111111"; -- OFF (A-F or error)
-            end case;
-        else
-            segments <= (others => '1');
-        end if;
+        case digit is
+            when "0000" => segments <= "1000000"; -- 0
+            when "0001" => segments <= "1111001"; -- 1
+            when "0010" => segments <= "0100100"; -- 2
+            when "0011" => segments <= "0110000"; -- 3
+            when "0100" => segments <= "0011001"; -- 4
+            when "0101" => segments <= "0010010"; -- 5
+            when "0110" => segments <= "0000010"; -- 6
+            when "0111" => segments <= "1111000"; -- 7
+            when "1000" => segments <= "0000000"; -- 8
+            when "1001" => segments <= "0010000"; -- 9
+            when "1010" => segments <= "0001000"; -- A
+            when "1011" => segments <= "0000011"; -- B
+            when "1100" => segments <= "1000110"; -- C
+            when "1101" => segments <= "0100001"; -- D
+            when "1110" => segments <= "0000110"; -- E
+            when "1111" => segments <= "0001110"; -- F
+            when others => segments <= "1111111"; -- Off
+        end case;
     end process;
 end Behavioral;
